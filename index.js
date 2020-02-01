f9549c04fd2cff55d6a3c32e0335cfce5733fad1
lobana alarabi <loubanaeng@gmail.com>
2020-02-01 21:58:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+function findMatching( drivers,str)
+{
+  return drivers.filter(drive =>{
+    
+    return drive.toUpperCase() === str.toUpperCase()});
+}
+
+function fuzzyMatch(drivers,str)
+{
+  
+  return drivers.filter(drive => {
+    let all=[];
+    let i;
+      for(i=0;i<str.length ;i++ ){
+          if(drive.charAt(i).toUpperCase() != str.charAt(i).toUpperCase())
+          break;
+          
+          }
+          if(i==str.length)
+          {all.push(drive);}
+     return all;
+  });
+}
+
+function matchName(drivers,str)
+{
+  
+}
+
+findMatching(drivers,"Bobby");
+findMatching(drivers, 'Susan');
+fuzzyMatch(drivers, 'sa');
